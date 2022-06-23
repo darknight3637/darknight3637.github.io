63db70b174e61052d7f2479f946335beacc0948d
Akash <72139998+darknight3637@users.noreply.github.com>
2022-06-23 22:35:51 +0530

    Final year project

---
+++ ISP/src/com/graph/chart.java
@@ -0,0 +1,20 @@
+package com.graph;
+
+public class chart {
+	private String location;
+	private int count;
+	public String getlocation() {
+		return location;
+	}
+	public void setcloudserver(String location) {
+		this.location = location;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+
+}
---
+++ ISP/src/com/profit/profitchart.java
@@ -0,0 +1,20 @@
+package com.profit;
+
+public class profitchart {
+	private String server;
+	private int sum;
+	public String getserver() {
+		return server;
+	}
+	public void setserver(String server) {
+		this.server = server;
+	}
+	public int getsum() {
+		return sum;
+	}
+	public void setsum(int sum) {
+		this.sum = sum;
+	}
+
+
+}
